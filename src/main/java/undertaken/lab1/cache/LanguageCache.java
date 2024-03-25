acd1fdb6ef7e5103b4f68f5b47f22692e228df68
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-03-25 06:44:59 +0300

    1

--- src/main/java/undertaken/lab1/cache/LanguageCache.java
+++ src/main/java/undertaken/lab1/cache/LanguageCache.java
@@ -1,22 +1,29 @@
 package undertaken.lab1.cache;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LanguageCache {
-    private final Map<String, String> cache = new ConcurrentHashMap<>();
+    private final static int MAX_CACHE_SIZE = 3;
+
+    private final Map<String, String> cache = new LinkedHashMap<String, String>(MAX_CACHE_SIZE, 0.75f, true) { //коэф загрузки
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+            return size() > MAX_CACHE_SIZE;
+        }
+    };
 
     private final Logger logger = LoggerFactory.getLogger(LanguageCache.class);
 
-    public String get(String text) {
+    public synchronized String get(String text) {
         return cache.get(text);
     }
 
-    public void put(String text, String language) {
+    public synchronized void put(String text, String language) { //синхронизирован(только один поток может выполнять эту функцию)
         cache.put(text, language);
     }
 
