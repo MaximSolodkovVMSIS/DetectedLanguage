0b07cb01e714641a5e3b32572517fba4d8c046bd
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-03-25 07:58:08 +0300

    1

--- src/main/java/undertaken/lab1/cache/LanguageCache.java
+++ src/main/java/undertaken/lab1/cache/LanguageCache.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LanguageCache {
-    private final static int MAX_CAHCE_SIZE = 3;
+    private static final int MAX_CAHCE_SIZE = 3;
 
     private final Map<String, String> cache = new LinkedHashMap<String, String>(MAX_CAHCE_SIZE, 0.75f, true) { //коэф загрузки
         @Override
