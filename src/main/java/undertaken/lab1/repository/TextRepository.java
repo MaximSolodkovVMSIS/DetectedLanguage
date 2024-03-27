7cf7c94fb231d4a74705e62ff611f3f02a59120b
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-03-27 16:27:47 +0300

    1

--- src/main/java/undertaken/lab1/repository/TextRepository.java
+++ src/main/java/undertaken/lab1/repository/TextRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import undertaken.lab1.entity.Text;
 
+
 public interface TextRepository extends JpaRepository<Text, Long> {
+
+    //@Query(value = "SELECT t.* FROM text t JOIN language l ON t.language_id = l.id WHERE l.name = :languageName", nativeQuery = true)
     @Query("SELECT tl FROM Text tl WHERE tl.language.name = :languageName")
     List<Text> findAllByTextLanguageName(@Param("languageName") String languageName);
 
     Text findByText(String text);
 }
-
 
